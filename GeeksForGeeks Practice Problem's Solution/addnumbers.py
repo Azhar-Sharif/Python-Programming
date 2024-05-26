7ffaf9759471b0a1c4d4d18c6a35ef50434039bd
Azhar <azharsharif042@gmail.com>
2024-05-26 23:22:54 +0500

    commit

---
+++ GeeksForGeeks Practice Problem's Solution/addnumbers.py
@@ -0,0 +1,9 @@
+# Program to add Two Numbers
+def add_num(a,b):
+    print(a+b)
+def main():
+    a = int(input("Enter 1st number: "))
+    b = int(input("Enter 2nd numer: "))
+    add_num(a,b)
+
+main()
