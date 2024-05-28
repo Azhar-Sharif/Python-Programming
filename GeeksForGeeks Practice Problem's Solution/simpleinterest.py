82adbbc486ef68530b7e0f83d546d0fa2cb61e00
Azhar <161110566+Azhar-Sharif@users.noreply.github.com>
2024-05-28 16:35:15 +0500

    Add files via upload

---
+++ GeeksForGeeks Practice Problem's Solution/simpleinterest.py
@@ -0,0 +1,10 @@
+def simpleinterest(a):
+    interset_percent = float(10)
+    interest_rate = (a*interset_percent//100)
+    simpleinterest = a+interest_rate
+    print("Total amount after ",interest_rate," interest: ",simpleinterest)
+def main():
+    a = int(input("Enter price: "))
+    simpleinterest(a)
+
+main()
