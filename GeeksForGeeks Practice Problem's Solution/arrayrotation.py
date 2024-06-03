50f1fd56c00b35e497e76bb75145d8ec11e65856
Azhar <161110566+Azhar-Sharif@users.noreply.github.com>
2024-06-03 19:40:34 +0500

    Add files via upload

---
+++ GeeksForGeeks Practice Problem's Solution/arrayrotation.py
@@ -0,0 +1,12 @@
+from random import randint
+def array_rotation(arr):
+    tmp = arr[0]
+    for i in range(len(arr)-1):
+        arr[i] = arr[i+1]
+    arr[len(arr)-1] = tmp
+    return arr
+def main():
+    arr = [randint(1,20) for i in range(20)]
+    print(arr)
+    print(array_rotation(arr))
+main()
